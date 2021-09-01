74751d554e4f30faa402e8ba797613682ca59f20
Acer <Acer@DESKTOP-ANUJO1P>
2021-09-01 13:26:05 +0700

    Add project files.

---
+++ สัปดาห์5.1/Source.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+	int a,i,j;
+	scanf_s("%d",&a);
+	for (i = 0; i < a; i++) {
+			for (j = 0; j < a; j++) {
+				printf("*");
+			}
+			printf("\n");
+	}
+	return 0;
+}
